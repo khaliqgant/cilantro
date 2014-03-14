3459677ecb4290718b0741e0080ad6b89abc7dee
Byron Ruth <b@devel.io>
2014-03-13 21:07:49 -0400

    Fix wrong env name

--- bin/check_signoff.sh
+++ bin/check_signoff.sh
@@ -8,7 +8,7 @@ sudo apt-get install -qq python-yaml
 git remote add upstream git://github.com/cbmi/cilantro.git
 
 UPSTREAM=master
-CURRENT_DIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+DIRNAME=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 if [ "$TRAVIS_PULL_REQUEST" != false ]; then
     UPSTREAM=$TRAVIS_BRANCH;
@@ -16,4 +16,4 @@ fi;
 
 git fetch --append --no-tags upstream refs/heads/$UPSTREAM:refs/remotes/upstream/$UPSTREAM
 
-/usr/bin/python "$DIR/check_signoff.py"
+/usr/bin/python "$DIRNAME/check_signoff.py"
